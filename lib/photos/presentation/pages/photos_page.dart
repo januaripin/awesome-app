429c0c95a2a17b4701d14694c060792e49583485
Yanuar Arifin <halo@yanuar.id>
2021-11-21 23:31:53 +0700

    add photo item widget, photo and photo src entity, and equatable cached_network_image package

--- lib/photos/presentation/pages/photos_page.dart
+++ lib/photos/presentation/pages/photos_page.dart
@@ -1,5 +1,8 @@
+import 'package:awesome_app/photos/domain/entities/photo.dart';
+import 'package:awesome_app/photos/domain/entities/photo_src.dart';
 import 'package:awesome_app/photos/presentation/controllers/icon_controller.dart';
 import 'package:awesome_app/photos/presentation/widgets/app_bar_icon_button.dart';
+import 'package:awesome_app/photos/presentation/widgets/photo_item.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -26,8 +29,25 @@ class PhotosPage extends StatelessWidget {
           ),
         ],
       ),
-      body: const CustomScrollView(
-        slivers: [],
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: PhotoItem(
+                photo: Photo(
+                  id: 1,
+                  width: 100,
+                  height: 100,
+                  photographerId: 1,
+                  src: PhotoSrc(
+                    large: 'https://via.placeholder.com/720x920?text=Awesomee',
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
